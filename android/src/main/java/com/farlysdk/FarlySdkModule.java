ce923d793de9310aa4efd60598f62205f42ac652
Philippe Auriach <p.auriach@gmail.com>
2023-04-03 19:57:55 +0200

    chore: initial commit

---
+++ android/src/main/java/com/farlysdk/FarlySdkModule.java
@@ -0,0 +1,32 @@
+package com.farlysdk;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = FarlySdkModule.NAME)
+public class FarlySdkModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "FarlySdk";
+
+  public FarlySdkModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  FarlySdkExample
+//
+
+import Foundation
