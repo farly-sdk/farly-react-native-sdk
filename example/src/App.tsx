d7dc4b92d5a28388e5d6bd1b8003e9fd4a4369d3
Philippe Auriach <p.auriach@gmail.com>
2023-04-04 14:04:46 +0200

    Usage example initialization

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,27 +1,149 @@
+/* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-farly-sdk';
+import {
+  Button,
+  Image,
+  Linking,
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import Farly from 'react-native-farly-sdk';
+import type { FeedElement, OfferWallRequest } from 'src/types';
+
+const request: OfferWallRequest = {
+  userId: '123',
+  // zipCode: '75017',
+  // countryCode: 'FR',
+  // userAge: 32,
+  // userGender: 'Male',
+  // userSignupDate: new Date('2021-01-01 03:12:32'),
+  // callbackParameters: ['first', 'second'],
+};
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [farlySetup, setFarlySetup] = React.useState(false);
+  const [isAdvertisingAuthorized, setIsAdvertisingAuthorized] =
+    React.useState(false);
+  const [url, setUrl] = React.useState('');
+  const [offerwallOffers, setOfferwallOffers] = React.useState<FeedElement[]>(
+    []
+  );
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    Farly.setup({
+      apiKey: '',
+      publisherId: '',
+    }).then(() => {
+      console.log('FarlySdk did setup');
+      setFarlySetup(true);
+    });
   }, []);
 
+  React.useEffect(() => {
+    Farly.requestAdvertisingIdAuthorization().then((isAuthorized) => {
+      console.log('isAuthorized', isAuthorized);
+      setIsAdvertisingAuthorized(isAuthorized);
+    });
+  }, []);
+
+  React.useEffect(() => {
+    if (farlySetup) {
+      Farly.getHostedOfferwallUrl(request).then((_url) => {
+        setUrl(_url ?? '');
+      });
+    }
+  }, [farlySetup]);
+  React.useEffect(() => {
+    if (farlySetup) {
+      Farly.getOfferwall(request).then((offerwall) => {
+        setOfferwallOffers(offerwall);
+      });
+    }
+  }, [farlySetup]);
+
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <Text style={{ fontSize: 17 }}>
+            Farly setup: {farlySetup ? 'yes' : 'no'}
+          </Text>
+          <Text style={{ fontSize: 17 }}>
+            Advertising ID authorized: {isAdvertisingAuthorized ? 'yes' : 'no'}
+          </Text>
+          <View style={{ paddingVertical: 32 }}>
+            <Button
+              title="Show offerwall in browser"
+              onPress={() => Farly.showOfferwallInBrowser(request)}
+            />
+          </View>
+          <View style={{ paddingBottom: 32 }}>
+            <Button
+              title="Show offerwall in webview"
+              onPress={() => Farly.showOfferwallInWebview(request)}
+            />
+          </View>
+          <Text
+            style={{
+              fontSize: 17,
+              fontWeight: 'bold',
+              paddingHorizontal: 8,
+            }}
+          >
+            URL to open in browser or webview
+          </Text>
+          <Text style={{ paddingTop: 8 }} selectable>
+            {url}
+          </Text>
+        </View>
+        <View style={{ paddingTop: 32 }}>
+          <Text
+            style={{
+              fontSize: 17,
+              fontWeight: 'bold',
+              paddingHorizontal: 8,
+            }}
+          >
+            {offerwallOffers.length} offers fetched programatically
+          </Text>
+          {offerwallOffers.map((offer) => (
+            <Pressable
+              key={offer.id}
+              style={{
+                flexDirection: 'row',
+                paddingVertical: 4,
+                borderBottomWidth: 1,
+              }}
+              onPress={() => Linking.openURL(offer.link)}
+            >
+              <Image
+                source={{ uri: offer.icon }}
+                style={{ width: 40, height: 40 }}
+              />
+              <View style={{ flex: 1, paddingHorizontal: 8 }}>
+                <Text style={{ fontWeight: 'bold' }}>{offer.name}</Text>
+                <Text>{offer.smallDescription}</Text>
+                <Text style={{ fontWeight: 'bold' }}>
+                  {offer.actions[0]?.text}
+                </Text>
+              </View>
+            </Pressable>
+          ))}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 8,
   },
   box: {
     width: 60,
